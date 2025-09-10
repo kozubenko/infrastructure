7cadb6ab4148cf6f13464851b82bae2326c073bb
kozubenko <andrii.kozubenko@gmail.com>
2025-09-10 14:55:52 +0300

    add remote state

--- ops/infrastructure/main.tf
+++ ops/infrastructure/main.tf
@@ -1,7 +1,14 @@
 module "gke_cluster" {
-  source         = "github.com/kozubenko/terraform-google-gke-cluster"
-  
-  GOOGLE_PROJECT = "infrastructure-demo-project"
-  GKE_NUM_NODES  = 2
+  source = "github.com/kozubenko/terraform-google-gke-cluster"
+
+  GOOGLE_PROJECT   = "infrastructure-demo-project"
+  GKE_NUM_NODES    = 2
   GKE_CLUSTER_NAME = "demo"
 }
+terraform {
+  backend "gcs" {
+    bucket = "kozubenko-demo-state"
+    prefix = "terraform/state"
+  }
+}
+  
